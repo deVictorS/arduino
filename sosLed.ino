a7fb9bc68c3989077cef91e19275438d01760043
deVictorS <victorsantos3245@gmail.com>
2025-05-20 23:06:21 -0300

    sosLed

---
+++ sosLed.ino
@@ -0,0 +1,51 @@
+void setup() {
+    
+  pinMode(7, OUTPUT);
+  
+}
+
+void loop() {
+  digitalWrite(7, HIGH); //letra S
+  delay(200);
+  digitalWrite(7, LOW);
+  delay(200);
+  digitalWrite(7, HIGH);
+  delay(200);
+  digitalWrite(7, LOW);
+  delay(200);
+  digitalWrite(7, HIGH);
+  delay(200);
+  
+  digitalWrite(7, LOW); //delay entre códigos
+  delay(1000);
+
+  digitalWrite(7, HIGH); //letra o
+  delay(800);
+  digitalWrite(7, LOW);
+  delay(800);
+  digitalWrite(7, HIGH);
+  delay(800);
+  digitalWrite(7, LOW);
+  delay(800);
+  digitalWrite(7, HIGH);
+  delay(800);
+
+  digitalWrite(7, LOW); //delay entre códigos
+  delay(1000);
+
+  digitalWrite(7, HIGH); //letra S
+  delay(200);
+  digitalWrite(7, LOW);
+  delay(200);
+  digitalWrite(7, HIGH);
+  delay(200);
+  digitalWrite(7, LOW);
+  delay(200);
+  digitalWrite(7, HIGH);
+  delay(200);
+  
+  digitalWrite(7, LOW); //delay entre códigos
+  delay(3000);
+
+   
+}
